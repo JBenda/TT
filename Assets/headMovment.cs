275ccf44396ef4750e21cca3e7c1c4549164405a
Julian Benda <Zabuza98@gmx.net>
2018-04-13 23:03:34 -0700

    inital Commit

---
+++ Assets/headMovment.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class headMovment : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+        transform.position = new Vector3(0.0f, 0.0f, 0.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float ah = Input.GetAxis("Horizontal");
+        float av = Input.GetAxis("Vertical");
+        transform.position += new Vector3(ah, av, 0.0f);
+	}
+}
