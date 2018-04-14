63a8084ef25bb65884ef6f9a07b0bc6a59ded4e9
Sigurd Knarhøi Johannsen <sigurd_johannsen@hotmail.com>
2018-04-14 20:04:15 +0200

    adds new images. adds main menu functionality. adds some folder structure.

---
+++ Assets/Scripts/MainMenuPressButton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/*
+    This script allows the player to press the StartGame and the Quit buttons in the main menu.
+    */
+public class MainMenuPressButton : MonoBehaviour {
+
+    private MainMenuMoveCursor _cursorPosition;
+
+    void Start () {
+        _cursorPosition = FindObjectOfType<MainMenuMoveCursor>();
+    }
+
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space) && _cursorPosition.currentPointerPosition == 1) {
+            StartCoroutine(LoadYourAsyncScene());
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && _cursorPosition.currentPointerPosition == 0) {
+            Application.Quit();
+        }
+    }
+
+    IEnumerator LoadYourAsyncScene() {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Assets/Scenes/Julian.unity");
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone) {
+            yield return null;
+        }
+    }
+}
