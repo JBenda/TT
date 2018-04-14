55c759fe43a83c96498e00d70ee0015bd5c0e24c
Julian Benda <Zabuza98@gmx.net>
2018-04-14 13:31:40 -0700

    Added Button input with time Counter

---
+++ Assets/CountDown.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDown : MonoBehaviour {
+    public Image circle;
+    public Text text;
+    private float fill;
+    private float time;
+	// Use this for initialization
+	void Start () {
+        fill = 0.0f;
+        circle.fillAmount = fill;
+	}
+    public void StopCounter()
+    {
+        fill = .0f;
+        circle.fillAmount = fill;
+        text.enabled = false;
+    }
+	public void StartCounter(float t)
+    {
+        time = t;
+        fill = 1.0f;
+        circle.fillAmount = fill;
+        text.enabled = true;
+    }
+	// Update is called once per frame
+	void Update () {
+        if (fill > 0.0f)
+        {
+            fill -= 1.0f / time * Time.deltaTime;
+            circle.fillAmount = fill;
+        }
+        else if (text.enabled)
+            text.enabled = false;
+	}
+}
---
+++ Assets/WallOne.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class WallOne : MonoBehaviour {
+    private bool pressed = false;
+    public CountDown counter;
+    public CameraMovment cam;
+    public Slider health;
+    public int GamificationLvl;
+    private bool waterFlow;
+    private float live = 1.0f;
+    private bool holdBreath;
+    private float punsh = -1.0f;
+    // Use this for initialization
+    void Start() {
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (punsh > 0)
+        {
+            punsh -= Time.deltaTime;
+            if (Input.GetKeyDown(KeyCode.O))
+            {
+                counter.StopCounter();
+                punsh = -1.0f;
+            }
+            if (punsh <= .0f && punsh > -1.0f)
+            {
+                live -= 0.5f;
+            }
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            live -= 0.1f * Time.deltaTime;
+        }
+        else if (waterFlow)
+        {
+            live -= 0.2f * Time.deltaTime;
+        }
+        health.value = live;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (waterFlow) waterFlow = false;
+            else waterFlow = true;
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            punsh = 5.0f;
+            counter.StartCounter(punsh);
+        }
+    }
+}
