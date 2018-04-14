6dfd0ad06eadf13f25889514a937dfe918f8b462
Sigurd Knarhøi Johannsen <sigurd_johannsen@hotmail.com>
2018-04-14 18:44:24 +0200

    wip

---
+++ Assets/Scripts/MainMenuAnimateElement.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+/*
+    This script animates menu items in the main menu that are currently active.
+    */
+
+public class MainMenuAnimateElement : MonoBehaviour {
+    public int menuItemId;
+
+    private Transform _transform;
+    private MainMenuMoveCursor _cursorPosition;
+
+    //
+    public Vector3 startScale,
+                   targetScale;
+    public float speed = 1.0f;
+    private float startTime,
+                  journeyLength,
+                  fracJourney;
+    bool forward = true;
+
+    void Start () {
+        _transform = GetComponent<Transform>();
+        _cursorPosition = FindObjectOfType<MainMenuMoveCursor>();
+
+        //
+        startTime = Time.time;
+        if (forward)
+        {
+            journeyLength = Vector3.Distance(startScale, targetScale);
+        }
+        else { return; }
+    }       
+
+    void Update() {
+        if (_cursorPosition.currentPointerPosition == menuItemId) {
+            float distCovered = (Time.time - startTime) * speed;
+            fracJourney = distCovered / journeyLength;
+            Vector3 currentScale = Vector3.Lerp(startScale, targetScale, fracJourney);
+            transform.localScale = new Vector3(currentScale.x, currentScale.y, currentScale.z);
+        }
+
+        if (transform.localScale == targetScale) {
+
+        }
+
+        else {
+            _transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+    }
+}
---
+++ Assets/Scripts/MainMenuMoveCursor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+/*
+    This script allows the player to move the cursor up and down in the main menu.
+    */
+public class MainMenuMoveCursor : MonoBehaviour {
+
+    public int currentPointerPosition;
+	void Start () {
+        currentPointerPosition = 1;
+	}
+	
+	void Update () {
+        if (Input.GetKeyDown("up") && currentPointerPosition == 0) {
+            currentPointerPosition = 1;
+        }
+        if (Input.GetKeyDown("down") && currentPointerPosition == 1) {
+            currentPointerPosition = 0;
+        }
+    }
+}
