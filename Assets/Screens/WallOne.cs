95b9f703c145d9dbbd9f1d09a48982744b6f4941
Julian Benda <Zabuza98@gmx.net>
2018-04-14 15:26:59 -0700

    prototype Water

--- Assets/Screens/WallOne.cs
+++ Assets/Screens/WallOne.cs
@@ -4,20 +4,51 @@ using UnityEngine.UI;
 using UnityEngine;
 
 public class WallOne : MonoBehaviour {
-    private bool pressed = false;
     public CountDown counter;
+    public Text info;
     public Slider health;
     public int GamificationLvl;
     private bool waterFlow;
+    private float waterTime = -1.0f;
     private float live = 1.0f;
     private bool holdBreath;
     private float punsh = -1.0f;
+    private int eventQuote = 5;
     // Use this for initialization
     void Start() {
+    }
+    private void Hit()
+    {
+        punsh = 5.0f;
+        counter.StartCounter(punsh);
     }
     // Update is called once per frame
     void Update()
     {
+        if (waterFlow)
+        {
+            info.text = "Water";
+        }
+        else
+        {
+            info.text = "NO";
+        }
+        if (punsh < 0 && !waterFlow)
+        {
+            if (Random.Range(0, 100) < eventQuote)
+            {
+                int rnd = Random.Range(0 ,100);
+                if (rnd < 40)
+                {
+                    Hit();
+                }
+                else
+                {
+                    waterFlow = true;
+                    waterTime = rnd / 50;
+                }
+            }
+        }
         if (punsh > 0)
         {
             punsh -= Time.deltaTime;
@@ -31,9 +62,17 @@ public class WallOne : MonoBehaviour {
                 live -= 0.5f;
             }
         }
+        if (waterTime > 0)
+        {
+            waterTime -= Time.deltaTime;
+        }
+        else
+        {
+            waterFlow = false;
+        }
         if (Input.GetKey(KeyCode.A))
         {
-            live -= 0.1f * Time.deltaTime;
+            live -= 0.02f * Time.deltaTime;
         }
         else if (waterFlow)
         {
@@ -47,8 +86,7 @@ public class WallOne : MonoBehaviour {
         }
         if (Input.GetKeyDown(KeyCode.P))
         {
-            punsh = 5.0f;
-            counter.StartCounter(punsh);
+            this.Hit();
         }
     }
 }
